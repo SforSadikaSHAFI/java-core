f5f9000ae58adacb4f23b2ab3ab0d18b86ae7acc
SforSadikaSHAFI <medicaregrp7@gmail.com>
2022-01-04 03:59:51 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //  System.out.println("I love you Rahat..thank you so much for everything.");
+	// write your code here
+    }
+}
